942ac91d9f984df58a72348dc56bdc8ef522fc3f
rahul-infra <rahul.sayam@infraspec.dev>
2026-01-12 12:28:09 +0530

    fix: Update s3 duplicate variables.

--- components/terraform/s3-bucket/versions.tf
+++ components/terraform/s3-bucket/versions.tf
@@ -8,25 +8,3 @@ terraform {
     }
   }
 }
-
-# Configure the AWS provider using the region variable
-provider "aws" {
-  region = var.region
-}
-
-# Additional variables passed by Atmos from stack config
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "stage" {
-  description = "Stage name (e.g., dev, prod)"
-  type        = string
-}
-
-variable "tags" {
-  description = "Common tags to apply to all resources"
-  type        = map(string)
-  default     = {}
-}
